ad6d1eb8d1dfe49e5254144aa4991873b0a40b84
bhavanampisay <147297107+bhavanampisay@users.noreply.github.com>
2024-09-22 20:27:29 +0530

    Add files via upload
    This is an interactive navigation menu that changes color or style when scrolled or when hovering over a menu item.

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript for sticky navbar effect on scroll
+window.addEventListener("scroll", function() {
+    var navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+});
